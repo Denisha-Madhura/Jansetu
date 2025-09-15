c10bb82b9b0011877d9fdec051beadaeb3a9b873
Aditya Jyoti <aj.adityajyoti@gmail.com>
2025-09-15 23:01:00 +0530

    init: Create frontend project

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Jan Setu",
+  description: "A crowd-sourced platform for reporting civic issues.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className="antialiased">{children}</body>
+    </html>
+  );
+}
